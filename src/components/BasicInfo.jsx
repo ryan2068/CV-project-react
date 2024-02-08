b550bc56b3d023f8336a4fff6f9dd7bec32373ff
ryan2068 <ryannkan@gmail.com>
2024-02-07 19:04:59 -0500

    Add: onChange handler & haven't lifted state yet

--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "/Users/ryan/OdinProject/cv-application-react/src/styles/styles.css"
 
-export default function BasicInfo() {
+export function BasicInfo() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("")
   const [phoneNumber, setPhoneNumber] = useState(0)
@@ -16,16 +16,32 @@ export default function BasicInfo() {
   return (
     <div className="content">
       <div className="form-div">
-        <form action="">
+        <form onChange={handleSubmit} action="">
           <label htmlFor="name">Please enter your name   </label>
           <input type="text" name="name" onChange={e => setName(e.target.value)} /> <br></br>
           <label htmlFor="email">Please enter your email   </label>
           <input type="email" name="email" onChange={e => setEmail(e.target.value)} /><br></br>
           <label htmlFor="phoneNumber">Please enter your phone number   </label>
           <input type="tel" name="phoneNumber" onChange={e => setPhoneNumber(e.target.value)}/><br></br>
-          <button type="submit" onClick={handleSubmit}></button>
         </form>
       </div>
     </div>
   )
 }
+
+function infoTyped (name, email, phoneNumber) {
+
+
+  return (
+    <>
+    
+    </>
+  ) 
+    
+
+}
+
+function Resume () {
+
+}
+
