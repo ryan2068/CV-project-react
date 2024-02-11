e574b4b55c8984d3fecbcf6336c6707af8e93616
ryan2068 <ryannkan@gmail.com>
2024-02-11 13:26:38 -0500

    last save with state lifted but not working correctly

--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 import "/Users/ryan/OdinProject/cv-application-react/src/styles/styles.css"
 
 function BasicInfo({ onChange, value }) {
-  const [email, setEmail] = useState("")
-  const [phoneNumber, setPhoneNumber] = useState(0)
+  
+
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(name)
-    console.log(email)
-    console.log(phoneNumber)
+    console.log(generalData.name)
+    console.log(generalData.email)
+    console.log(generalData.phoneNumber)
   }
   return (
     <div className="content">
@@ -19,21 +19,22 @@ function BasicInfo({ onChange, value }) {
           <label htmlFor="name">Please enter your name   </label>
           <input type="text" name="name" value={value} onChange={onChange}/><br></br>
           <label htmlFor="email">Please enter your email   </label>
-          <input type="email" name="email" onChange={e => setEmail(e.target.value)} /><br></br>
+          <input type="email" name="email" value={value} onChange={onChange} /><br></br>
           <label htmlFor="phoneNumber">Please enter your phone number   </label>
-          <input type="tel" name="phoneNumber" onChange={e => setPhoneNumber(e.target.value)}/><br></br>
+          <input type="tel" name="phoneNumber" value={value} onChange={onChange}/><br></br>
         </form>
       </div>
     </div>
   )
 }
 
-function InfoTyped ({onChange, value}) {
+function InfoTyped ({onChange, generalData, email}) {
 
 
   return (
     <>
-      <h3>{value}</h3>
+      <h3>{generalData}</h3>
+      <h3>{generalData}</h3>
     </>
   ) 
     
@@ -41,23 +42,43 @@ function InfoTyped ({onChange, value}) {
 }
 
 export function Resume () {
-  const [name, setName] = useState('')
+  const [generalData, setGeneralData] = useState({
+    name: '',
+    email: '',
+    phoneNumber: ''
+  })
 
-  function handleChange(e) {
-    setName(e.target.value)
-    console.log(name)
-    
+  function handleNameChange(e) {
+    setGeneralData({
+      ...generalData,
+      name: e.target.value
+    });
   }
+
+  function handleEmailChange(e) {
+    setGeneralData({
+      ...generalData,
+      email: e.target.value
+    });
+  }
+
+  function handlePhoneNumberChange(e) {
+    setGeneralData({
+      ...generalData,
+      phoneNumber: e.target.value
+    });
+  }
+
 
   return (
     <>
       <BasicInfo
-      value={name}
-      onChange={handleChange}
+      value={generalData}
+      onChange={handleNameChange}
       />
       <InfoTyped 
-      value={name}
-      onChange={handleChange}
+      value={generalData}
+      onChange={handleEmailChange}
       ></InfoTyped>
     </>
   )
